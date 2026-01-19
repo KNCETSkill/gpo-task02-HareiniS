0ef394133d24902460e502daa9da87ca84c0417c
HareiniS <hareiniharsa@gmail.com>
2026-01-19 15:54:56 +0530

    Refactor attendance evaluation logic
    Fix conditional structure and correct output method.

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -5,9 +5,9 @@ public class AttendanceStatusEvaluator{
     int a = c.nextInt();
     if(a>90){
       System.out.println("Excellent");
-      if(a>=60 && a<=85){
-        System.out.println("Satisfactory");
-      }
+    }
+    elseif(a>=60 && a<=85){
+      System.out.ptintln("Satisfactory");
     }
     else{
       System.out.println("Poor");
